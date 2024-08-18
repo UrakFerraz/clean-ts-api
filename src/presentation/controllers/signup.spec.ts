096ed3e5cbe0491995cee4b1345d5fe4eecb849f
UrakFerraz <urakmkt@yahoo.com.br>
2024-08-17 21:44:40 -0300

    test: ensure return 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SIgnUp Controller', () => {
+  test('should return status 400 if no name is provided ', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
